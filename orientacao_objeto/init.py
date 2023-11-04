7fff524f00a60f0ea0129aef8e8cff1afaef2f4d
DanielKulique <daniel_kulique@hotmail.com>
2023-11-03 21:52:05 -0300

    c completo ponteiro

--- orientacao_objeto/init.py
+++ orientacao_objeto/init.py
@@ -1,3 +1,13 @@
+# class - classes são moldes para criar novos objetos
+
+# as classes geram novos objetos (instancias) que 
+# podem ter seus proprias atributos e metodos
+
+# Os objetos gerados pela classe podem usar seus dados
+# internos para realizar varias ações
+
+# por convencao, usamos PascalCase para nomes de classos
+
 class Pessoa:
     def __init__(self, nome, sobrenome):
         self.nome = nome
