c2a1d67789d346d1f55a54116239bf87a706e7b8
DanielKulique <daniel_kulique@hotmail.com>
2023-11-18 16:01:25 -0300

    finalizando o campo minado

--- ponteiros/campo_minado.c
+++ ponteiros/campo_minado.c
@@ -122,7 +122,7 @@ void imprimir()
                 if(jogo[l][c].eBomba)
                     printf(" * ");
                 else
-                    printf( "%d ", jogo[l][c].vizinhos);
+                    printf( " %d ", jogo[l][c].vizinhos);
             }
             else 
                 printf("   ");
@@ -132,22 +132,86 @@ void imprimir()
     }
 }
 
+//procedimento para abrir a coordenada digitada pelo usuario
+void abrirCelula(int l, int c){
+    if(coordenadaEhvalida(l, c) == 1 && jogo[l][c].estaAberta == 0){
+        jogo[l][c].estaAberta = 1;
+        if(jogo[l][c].vizinhos == 0){
+            /*
+                l-1 e c acima
+                l+1 e c abaixo
+                l e c + 1 direita
+                l e c - 1 esquerda 
+            */
+        abrirCelula(l-1, c);
+        abrirCelula(l+1, c);
+        abrirCelula(l, c+1);
+        abrirCelula(l, c-1);
+        }
+    }
+}
+
+/*
+    funcao para verificar a vitoria
+    1 - ganhou
+    0 - nao ganhou ainda
+*/
+int ganhou() {
+    int quatidade = 0;
+
+    for(l = 0; l < tam; l++)
+    {
+        for(c = 0; c <tam; c++)
+        {
+            if(jogo[l][c].estaAberta == 0  && jogo[l][c].eBomba == 0)
+                quatidade++;
+        }
+    }
+    return quatidade;
+}
+
+
+
 //procedimento jogar que faz a leitura das coordenadas
 
 void jogar(){
     int linha, coluna;
 
-    printf("\nDigite as coordenadas de linha e coluna: ");
-    scanf("%d%d", &linha, &coluna);
+    do{
+        imprimir();
+        do{
+            printf("\nDigite as coordenadas de linha e coluna: ");
+            scanf("%d%d", &linha, &coluna); 
+
+            if(coordenadaEhvalida(linha, coluna) == 0 || jogo[linha][coluna].estaAberta == 1);
+                printf("\nCoordenada invalida ou ja aberta!");
+        }while(coordenadaEhvalida(linha, coluna) == 0 || jogo[linha][coluna].estaAberta == 1);
+
+        abrirCelula(linha, coluna);
+    }while(ganhou() != 0 && jogo[linha][coluna].eBomba == 0);
+
+    if(jogo[linha][coluna].eBomba == 1)
+        printf("\n\tQue pena! Voce perdeu!\n");
+    else
+        printf("\n\tPARABENS! VOCE GANHOU!\n");
+    imprimir();
 }
 
 int main()
 {
-    inicializarJogo();
-    sortearBombas(10);
-    contarBombas();
-    printf("\n\t\t\tCAMPO MINADO\n");
-    imprimir();
+    int opcao;
+
+    do{
+        inicializarJogo();
+        sortearBombas(10);
+        contarBombas();
+        printf("\n\t\t\tCAMPO MINADO\n");
+        jogar();
+
+        printf("\nDigite 1 para jogar novamente: ");
+        scanf("%d", &opcao);
+
+    }while(opcao == 1);
 
     return 0;
 }
