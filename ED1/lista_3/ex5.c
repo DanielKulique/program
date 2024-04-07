03a161bd887e1d1a5f9d01871f74a4a82dd298a7
danielkulique <daniel_kulique@hotmail.com>
2024-04-07 15:42:11 -0300

    ex calculo auxilio pronto

--- ED1/lista_3/ex5.c
+++ ED1/lista_3/ex5.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct{
 	char nome[20];
@@ -7,16 +8,67 @@ typedef struct{
 	float renda_bruta;
 }Pessoa;
 
-typedef struct{
-	Pessoa *pessoa;
-	int quantidade_familia;
-}Residencia;
-
+void entrada_dados(Pessoa *familia, int membros);
+void calculo(Pessoa *familia, int membros);
+ 
 int main(){
+	int quant;
 	
-	Residencia um;
+	printf("\nDigite quantos membros tem na familia: ");
+	scanf("%d", &quant);
+	setbuf(stdin, NULL);
 	
-	um.pessoa[2];
+	Pessoa *Familia; //crio um ponteiro do tipo familia
 	
+	Familia = (Pessoa *)malloc(quant*sizeof(Pessoa)); // aloco dinamicamente esse ponteiro;
+	
+	if(!Familia){
+		printf("\nErro Alocacao");
+		return -1;
+	}
+	
+	entrada_dados(Familia, quant); //passo o ponteiro por referencia para a funcao entra_dados;
+	calculo(Familia, quant);	
+	
+	free(Familia);
+	Familia = NULL;
+
 	return 0;
+
+}
+
+void entrada_dados(Pessoa *familia, int membros){ //manipula entrada de dados de um ponteiro tipo estrutura Pessoa
+	int i=0;
+	for(i=0;i<membros;i++){
+		printf("\nMembro %d", i+1);
+		printf("\nDigite o Nome: ");
+		fgets((familia+i)->nome, 20, stdin);
+		(familia+i)->nome[strcspn((familia+i)->nome, "\n")] = '\0';
+		
+		printf("Digite a ocupacao: ");
+		fgets((familia+i)->ocupacao, 10, stdin);
+		(familia+i)->ocupacao[strcspn((familia+i)->ocupacao, "\n")] = '\0';
+		
+		printf("Digite a Renda Bruta: ");
+		scanf("%f", &(familia+i)->renda_bruta);
+		setbuf(stdin, NULL);
+	}
+}
+
+void calculo(Pessoa *familia, int membros){
+	int soma_renda = 0, i=0, per_capita=0;
+	
+	for (i=0;i<membros; i++){
+		soma_renda += (familia+i)->renda_bruta;
+	}
+	per_capita = soma_renda/membros;
+	if(per_capita<1953){
+		printf("\nEstudante pode receber o auxilio");
+	}
+	else
+		printf("\nEstudante nao apto ao auxilio");
+	
+	
+
+
 }
